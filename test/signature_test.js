9c5504bf01ce6111edd9f549704aec6ddca5e9c4
Taka Okunishi <okunishinishi@gmail.com>
2017-04-29 21:59:25 +0900

    Version incremented to 1.0.3

---
+++ test/signature_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for signature.
+ * Runs with mocha.
+ */
+'use strict'
+
+const signature = require('../lib/signature.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('signature', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Signature', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
